b8a4dc4ba59ea5ab792315c6c04abea06c05f71b
slesi2002-svg <slesi2002@gmail.com>
2026-08-08 20:39:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
-const VERSION='airbank-demo-v5-input-centered';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil((async()=>{for(const k of await caches.keys()) await caches.delete(k); await self.clients.claim();})()));
+const VERSION='airbank-demo-v6-stable-layout';
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => event.waitUntil((async()=>{
+  for (const key of await caches.keys()) await caches.delete(key);
+  await self.clients.claim();
+})()));
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request, {cache:'no-store'}));
+  }
+});
