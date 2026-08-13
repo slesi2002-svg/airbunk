93ab18fb668f83fc158d6c415dea1ac07ee94fc9
slesi2002-svg <slesi2002@gmail.com>
2026-08-13 16:20:32 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='airbank-demo-v11-high-balance-fix';
+const VERSION='airbank-demo-v12-pay-me';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => event.waitUntil((async()=>{
   for (const key of await caches.keys()) await caches.delete(key);
